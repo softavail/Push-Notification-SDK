4a077d2f9fefc32275351607425b9ec396b2aaaf
Asen Lekov <asenlekoff@gmail.com>
2016-05-16 23:02:17 +0300

    Initial ios structure

---
+++ ios/sdk/sdk/push.h
@@ -0,0 +1,15 @@
+//
+//  push.h
+//  push
+//
+//  Created by Asen Lekov on 5/16/16.
+//  Copyright © 2016 softavail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface push : NSObject
+
+-(void) helloworld;
+
+@end
---
+++ ios/sdk/sdk/push.m
@@ -0,0 +1,17 @@
+//
+//  push.m
+//  push
+//
+//  Created by Asen Lekov on 5/16/16.
+//  Copyright © 2016 softavail. All rights reserved.
+//
+
+#import "push.h"
+
+@implementation push
+
+-(void) helloworld {
+    NSLog(@"Hello World!");
+}
+
+@end
