ebc07d1d4de75b4f924ba7a63478e212d15f6db3
Asen Lekov <asenlekoff@gmail.com>
2016-05-24 16:28:11 +0300

    Push sample showing token.

---
+++ android/app/src/main/java/com/softavail/scg/push/demo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.softavail.scg.push.demo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final TextView token = (TextView) findViewById(R.id.token);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                token.setText(FirebaseInstanceId.getInstance().getToken());
+            }
+        });
+    }
+}
