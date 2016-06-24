23c91478eb0234008389e05c87a0c34fd692c8d5
Asen Lekov <asenlekoff@gmail.com>
2016-06-24 22:15:21 +0300

    Android: Better SDK error messages.

--- android/sdk/src/main/java/com/softavail/scg/push/sdk/ScgClient.java
+++ android/sdk/src/main/java/com/softavail/scg/push/sdk/ScgClient.java
@@ -1,10 +1,14 @@
 package com.softavail.scg.push.sdk;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.softavail.scg.push.sdk.ScgRestService.RegisterRequest;
 import com.softavail.scg.push.sdk.ScgRestService.UnregisterRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.logging.Level;
@@ -33,6 +37,7 @@ public class ScgClient {
 
     private static ScgClient sInstance;
     private static Level sLevel;
+    public static final String TAG = "ScgClient";
 
     private ScgClient(Context application, String rootUrl, String appId) {
         fApplication = application;
@@ -54,8 +59,7 @@ public class ScgClient {
         }
     }
 
-    public static boolean isInitialized()
-    {
+    public static boolean isInitialized() {
         return sInstance != null;
     }
 
@@ -143,11 +147,7 @@ public class ScgClient {
         mService.registerPushToken(request).enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
-                if (response.code() > 400) {
-                    if (result != null) result.onFailed(response.code(), response.message());
-                } else {
-                    if (result != null) result.onSuccess(response.code(), response.message());
-                }
+                sendResult(response, result);
             }
 
             @Override
@@ -168,11 +168,7 @@ public class ScgClient {
         mService.unregisterPushToken(request).enqueue(new Callback<ResponseBody>() {
             @Override
             public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
-                if (response.code() > 400) {
-                    if (result != null) result.onFailed(response.code(), response.message());
-                } else {
-                    if (result != null) result.onSuccess(response.code(), response.message());
-                }
+                sendResult(response, result);
             }
 
             @Override
@@ -180,6 +176,21 @@ public class ScgClient {
                 result.onFailed(-1, t.getMessage());
             }
         });
+    }
+
+    private void sendResult(retrofit2.Response<ResponseBody> response, ScgCallback result) {
+        if (response.code() > 400) {
+            if (result == null) return;
+
+            try {
+                JSONObject obg = new JSONObject(response.errorBody().string());
+                result.onFailed(obg.getInt("code"), obg.getString("description"));
+            } catch (Exception ignored) {
+                result.onFailed(response.code(), response.message());
+            }
+        } else {
+            if (result != null) result.onSuccess(response.code(), response.message());
+        }
     }
 
     /**
