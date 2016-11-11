ce64da50f4f9b6f89a9d98ce54f9edffbba67f36
Slav Sarafski <slav@spiritinvoker.com>
2016-11-11 15:33:10 +0200

    Demo app keep the accessToken, appID, baseURL, interaction report on/off FIxed crash after push received and keyboard were opened. UI fixes

--- ios/demo/demo/ViewController.swift
+++ ios/demo/demo/ViewController.swift
@@ -16,21 +16,45 @@ class ViewController: UIViewController {
     @IBOutlet weak var baseURIField: UITextField!
     @IBOutlet weak var appIDField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var reportSwith: UISwitch!
     
     var baseURL:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let defauts = UserDefaults.standard
         
-        baseURIField.text = "http://192.168.1.197:8080/scg-dra/proxy"
-        appIDField.text = "com.syniverse.push_demo"
+        if (defauts.object(forKey: "baseurl") == nil) {
+            defauts.set("http://192.168.1.197:8080/scg-dra/proxy", forKey: "baseurl")
+        }
+        if (defauts.object(forKey: "appid") == nil) {
+            defauts.set("com.syniverse.push_demo", forKey: "appid")
+        }
+        if (defauts.object(forKey: "token") != nil) {
+            accessTokenField.text = defauts.string(forKey: "token")
+        }
+        else {
+            accessTokenField.text = ""
+        }
+        if (defauts.object(forKey: "reporton") != nil) {
+            reportSwith.isOn = defauts.bool(forKey: "reporton")
+        }
+        else {
+            reportSwith.isOn = true
+            defauts.set(true, forKey: "reporton")
+        }
+        
+        baseURIField.text = defauts.string(forKey: "baseurl")
+        appIDField.text = defauts.string(forKey: "appid")
         
         SCGPush.instance.groupBundle = "group.com.softavail.scg.push.demo.group"
         SCGPush.instance.accessToken = "DQHlNta2J2QGHFHkI44Ei"
         SCGPush.instance.appID = "com.syniverse.push_demo"
         SCGPush.instance.callbackURI = "http://192.168.1.197:8080/scg-dra/proxy"
         SCGPush.instance.callbackURI = "http://95.158.130.102:8080/scg-dra/proxy/"
+        
+        
         //SCGPush.instance.callbackURI = "http://localhost:8912/scg-dra/proxy"
         //SCGPush.instance.callbackURI = "http://192.168.1.102:8912/scg-dra/proxy"
 //        SCGPush.instance.loadAttachment("7h0W48pb3XRmYT9L1TScc1", attachmentID: "LYT9CBu3TlIBLVgFP585J2", completionBlock: {
@@ -49,14 +73,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func textFieldChanged(_ sender: UITextField) {
+        let defauts = UserDefaults.standard
         if (sender.tag == 7){
             SCGPush.instance.accessToken = accessTokenField.text!
+            defauts.set(accessTokenField.text!, forKey: "token")
         }
         if (sender.tag == 8){
             SCGPush.instance.appID = appIDField.text!
+            defauts.set(appIDField.text!, forKey: "appid")
         }
         if (sender.tag == 9) {
             SCGPush.instance.callbackURI = baseURIField.text!
+            defauts.set(baseURIField.text!, forKey: "baseurl")
         }
     }
     
@@ -89,11 +117,17 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBAction func reportSwitchAction(_ sender: UISwitch) {
+        UserDefaults.standard.set(sender.isOn, forKey: "reporton")
+    }
+    
     func showAlert(_ title:String, mess:String){
         OperationQueue.main.addOperation {
             let alert = UIAlertController(title: title, message: mess, preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "Ok", style: .default, handler:nil))
-            self.present(alert, animated: true, completion: nil)
+            DispatchQueue.main.async {
+                self.present(alert, animated: true, completion: nil)
+            }
         }
     }
 }
